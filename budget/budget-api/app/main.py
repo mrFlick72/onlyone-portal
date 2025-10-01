e7e5c1a00f799de6c701611a37ce562be940d27c
mrflick72 <valerio.vaudi@gmail.com>
2025-10-01 07:36:27 +0200

    fix configuration loading and userfilter injector

--- budget/budget-api/app/main.py
+++ budget/budget-api/app/main.py
@@ -1,12 +1,13 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask 
 from infrastructure.management.HealthEndPoint import HealthEndPoint
 from infrastructure.middleware.UserNameInjectorFilter import UserNameInjectorFilter
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+load_dotenv(dotenv_path=os.getenv("BUDGET_API_CONFIG_FILE_LOCATION"))
+
 
 HealthEndPoint(app)
 user_name_injector_filter = UserNameInjectorFilter()
---
+++ budget/budget-api/local/start_py.sh
@@ -0,0 +1,8 @@
+export BUDGET_API_CONFIG_FILE_LOCATION="../local/.env"
+
+export AWS_ACCESS_KEY_ID="xxx" 
+export AWS_SECRET_ACCESS_KEY="xxx" 
+export AWS_REGION="eu-central-1"
+
+cd ../app
+python3 main.py
