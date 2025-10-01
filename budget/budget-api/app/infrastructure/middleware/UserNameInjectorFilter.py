3f79fe8aee7b8b12e82a740cbcfefe52dbec90c3
mrflick72 <valerio.vaudi@gmail.com>
2025-10-01 07:36:27 +0200

    fix configuration loading and userfilter injector

--- budget/budget-api/app/infrastructure/middleware/UserNameInjectorFilter.py
+++ budget/budget-api/app/infrastructure/middleware/UserNameInjectorFilter.py
@@ -13,7 +13,7 @@ class UserNameInjectorFilter:
     def __init__(self):
         self.user_name_resolver = LocalThreadUserNameResolver.get_instance()
         self.public_keys = {}
-        self.jwk_endpoint = f"{os.getenv('ACCOUNT_SERVICE_ISS')}/jwks"
+        self.jwk_endpoint = f"{os.getenv('IDP_ISS')}/oauth2/jwks"
         jwks = requests.get(self.jwk_endpoint).json()
         for jwk in jwks['keys']:
             kid = jwk['kid']
@@ -22,11 +22,9 @@ class UserNameInjectorFilter:
     def filter(self, user_name_claim="user_name"):
         if request.path not in ["/health"]:
             token = str(request.headers.get("authorization"))
-            print(token)
-            print(os.getenv('PHONE_BOOK_AUD_CLAIM'))
             decoded_token = decode(jwt=token, key=self.public_keys['123'], algorithms=['RS256'],
-                                   audience=os.getenv('PHONE_BOOK_AUD_CLAIM'))
-            self.user_name_resolver.set_user_name(decoded_token["user_name"])
+                                   audience=os.getenv('IDP_AUD_CLAIM'))
+            self.user_name_resolver.set_user_name(decoded_token[user_name_claim])
         return None
 
     def decode(self, token):
