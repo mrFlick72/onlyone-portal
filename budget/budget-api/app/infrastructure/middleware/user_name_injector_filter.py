84bff619b3e36b9a3fe005d56bddde355555d72c
mrflick72 <valerio.vaudi@gmail.com>
2025-10-02 22:01:45 +0200

    testing

--- budget/budget-api/app/infrastructure/middleware/user_name_injector_filter.py
+++ budget/budget-api/app/infrastructure/middleware/user_name_injector_filter.py
@@ -6,7 +6,10 @@ import requests
 from flask import request
 from jwt import get_unverified_header, decode
 
-from user.adapter.thread.LocalThreadUserNameResolver import LocalThreadUserNameResolver
+from user.adapter.thread.local_thread_user_name_resolver import (
+    LocalThreadUserNameResolver,
+)
+
 
 class UserNameInjectorFilter:
 
@@ -15,20 +18,26 @@ class UserNameInjectorFilter:
         self.public_keys = {}
         self.jwk_endpoint = f"{os.getenv('IDP_ISS')}/oauth2/jwks"
         jwks = requests.get(self.jwk_endpoint).json()
-        for jwk in jwks['keys']:
-            kid = jwk['kid']
-            self.public_keys[kid] = jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(jwk))
+        for jwk in jwks["keys"]:
+            kid = jwk["kid"]
+            self.public_keys[kid] = jwt.algorithms.RSAAlgorithm.from_jwk(
+                json.dumps(jwk)
+            )
 
     def filter(self, user_name_claim="user_name"):
         if request.path not in ["/health"]:
             token = str(request.headers.get("authorization"))
-            decoded_token = decode(jwt=token, key=self.public_keys['123'], algorithms=['RS256'],
-                                   audience=os.getenv('IDP_AUD_CLAIM'))
+            decoded_token = decode(
+                jwt=token,
+                key=self.public_keys["123"],
+                algorithms=["RS256"],
+                audience=os.getenv("IDP_AUD_CLAIM"),
+            )
             self.user_name_resolver.set_user_name(decoded_token[user_name_claim])
         return None
 
     def decode(self, token):
-        kid = get_unverified_header(token)['kid']
+        kid = get_unverified_header(token)["kid"]
         key = self.public_keys[kid]
 
-        return decode(token, key=key, algorithms=['RS256'])
+        return decode(token, key=key, algorithms=["RS256"])
---
+++ budget/budget-api/app/money/domain/money.py
@@ -0,0 +1,43 @@
+
+    # static final int SCALE_PRECISION = 2;
+    # static final int SCALE_CRITERIA = BigDecimal.ROUND_HALF_DOWN;
+
+    # public static final Money ZERO = Money.moneyFor("0.00");
+    # public static final Money ONE = Money.moneyFor("1.00");
+
+    # public Money(BigDecimal amount) {
+    #     this.amount = amount.setScale(SCALE_PRECISION, SCALE_CRITERIA);
+    # }
+
+    # public static Money moneyFor(String amount) {
+    #     return new Money(new BigDecimal(amount));
+    # }
+
+    # public Money plus(Money money) {
+    #     return new Money(this.amount.add(money.amount()).setScale(SCALE_PRECISION, SCALE_CRITERIA));
+    # }
+
+    # public String stringifyAmount() {
+    #     return amount.toString();
+    # }
+
+    # @Override
+    # public boolean equals(Object o) {
+    #     if (this == o) return true;
+    #     if (o == null || getClass() != o.getClass()) return false;
+    #     Money money = (Money) o;
+    #     return Objects.equals(amount, money.amount);
+    # }
+    
+    
+    
+import decimal
+
+
+class Money:
+    
+    def __init__(self, amount : decimal):
+        self.amount = amount
+        
+        
+    
