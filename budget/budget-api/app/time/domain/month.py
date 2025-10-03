a57ce1c0e7d52d1b78f0aaf02119fe5526dfd2b3
mrflick72 <valerio.vaudi@gmail.com>
2025-10-03 06:53:22 +0200

    full test coverage for date.py

---
+++ budget/budget-api/app/time/domain/month.py
@@ -0,0 +1,52 @@
+class Month:
+
+    def __init__(self, content: int):
+        self.content = content
+
+    @staticmethod
+    def JANUARY():
+        return Month(1)
+
+    @staticmethod
+    def FEBRUARY():
+        return Month(2)
+
+    @staticmethod
+    def MARCH():
+        return Month(3)
+
+    @staticmethod
+    def APRIL():
+        return Month(4)
+
+    @staticmethod
+    def MAY():
+        return Month(5)
+
+    @staticmethod
+    def JUNE():
+        return Month(6)
+
+    @staticmethod
+    def JULY():
+        return Month(7)
+
+    @staticmethod
+    def AUGUST():
+        return Month(8)
+
+    @staticmethod
+    def SEPTEMBER():
+        return Month(9)
+
+    @staticmethod
+    def OCTOBER():
+        return Month(10)
+
+    @staticmethod
+    def NOVEMBER():
+        return Month(11)
+
+    @staticmethod
+    def DECEMBER():
+        return Month(12)
