a6baf13619e65b97d1c53f4cbc3b164db9958f15
mrflick72 <valerio.vaudi@gmail.com>
2025-10-04 00:17:40 +0200

    money scaffoald and add Final type to some constants

--- budget/budget-api/app/money/domain/money.py
+++ budget/budget-api/app/money/domain/money.py
@@ -1,43 +1,58 @@
+# static final int SCALE_PRECISION = 2;
+# static final int SCALE_CRITERIA = BigDecimal.ROUND_HALF_DOWN;
 
-    # static final int SCALE_PRECISION = 2;
-    # static final int SCALE_CRITERIA = BigDecimal.ROUND_HALF_DOWN;
+# public static final Money ZERO = Money.moneyFor("0.00");
+# public static final Money ONE = Money.moneyFor("1.00");
 
-    # public static final Money ZERO = Money.moneyFor("0.00");
-    # public static final Money ONE = Money.moneyFor("1.00");
+# public Money(BigDecimal amount) {
+#     this.amount = amount.setScale(SCALE_PRECISION, SCALE_CRITERIA);
+# }
 
-    # public Money(BigDecimal amount) {
-    #     this.amount = amount.setScale(SCALE_PRECISION, SCALE_CRITERIA);
-    # }
+# public static Money moneyFor(String amount) {
+#     return new Money(new BigDecimal(amount));
+# }
 
-    # public static Money moneyFor(String amount) {
-    #     return new Money(new BigDecimal(amount));
-    # }
+# public Money plus(Money money) {
+#     return new Money(this.amount.add(money.amount()).setScale(SCALE_PRECISION, SCALE_CRITERIA));
+# }
 
-    # public Money plus(Money money) {
-    #     return new Money(this.amount.add(money.amount()).setScale(SCALE_PRECISION, SCALE_CRITERIA));
-    # }
+# public String stringifyAmount() {
+#     return amount.toString();
+# }
 
-    # public String stringifyAmount() {
-    #     return amount.toString();
-    # }
+# @Override
+# public boolean equals(Object o) {
+#     if (this == o) return true;
+#     if (o == null || getClass() != o.getClass()) return false;
+#     Money money = (Money) o;
+#     return Objects.equals(amount, money.amount);
+# }
 
-    # @Override
-    # public boolean equals(Object o) {
-    #     if (this == o) return true;
-    #     if (o == null || getClass() != o.getClass()) return false;
-    #     Money money = (Money) o;
-    #     return Objects.equals(amount, money.amount);
-    # }
-    
-    
-    
+
 import decimal
+from typing import Final
+
+SCALE_PRECISION: Final = 2
+SCALE_CRITERIA: Final = decimal.ROUND_HALF_DOWN
 
 
 class Money:
-    
-    def __init__(self, amount : decimal):
+
+    def __init__(self, amount: decimal):
         self.amount = amount
-        
-        
-    
+
+    @staticmethod
+    def money_for(amount: str):
+        pass
+
+    def stringify_amount():
+        pass
+
+    def plus(self, money: Money):
+        pass
+
+    def __eq__(self, other):
+        return self.amount == other.amount
+
+    def __hash__(self):
+        return hash((self.amount))
