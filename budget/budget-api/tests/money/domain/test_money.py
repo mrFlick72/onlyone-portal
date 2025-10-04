c234a32e9c8f8f3982c7008085cbf9e9770f3766
mrflick72 <valerio.vaudi@gmail.com>
2025-10-04 21:23:03 +0200

    add test coverage plugin and get a stringify version of the money object

--- budget/budget-api/tests/money/domain/test_money.py
+++ budget/budget-api/tests/money/domain/test_money.py
@@ -1,11 +1,16 @@
 from decimal import Decimal, getcontext
 from app.money.domain.money import Money
 
+
 def test_get_money_from_a_string():
     first_amount = Money.money_for("20.50")
     second_amount = Money.money_for("20.51")
     actual = first_amount.plus(second_amount)
-    print(actual)
-    print(Decimal("41.01"))
-    
+
     assert actual == Decimal("41.01")
+
+
+def test_get_money_stringify():
+    actual = Money.money_for("20.50")
+
+    assert actual.stringify_amount() == "20.50"
