e51c43d6af18ec3360873cb4cac43ba732a413f2
mrflick72 <valerio.vaudi@gmail.com>
2025-10-04 22:05:12 +0200

    app refactor use blueprint to create a new revenue endpoint

--- budget/budget-api/app/revenue/api/revenue_end_point.py
+++ budget/budget-api/app/revenue/api/revenue_end_point.py
@@ -2,7 +2,21 @@ from flask import Blueprint
 
 revenue_end_point = Blueprint('revenue_end_point',__name__)
 
-@revenue_end_point.route('/incomes', methods=['GET'])
-def get_incomes():
+@revenue_end_point.route('/budget/revenue', methods=['GET'])
+def get_revenue():
+    return {} 
+
+
+@revenue_end_point.route('/budget/revenue', methods=['POST'])
+def save_revenue():
+    return {} 
+
+@revenue_end_point.route('/budget/revenue/<id>', methods=['PUT'])
+def update_revenue():
+    return {} 
+
+
+@revenue_end_point.route('/budget/revenue/<id>', methods=['DELETE'])
+def delete_revenue():
     return {} 
 
