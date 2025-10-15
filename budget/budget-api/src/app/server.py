910d1bd29c6d1ae74c090146245ce0b3edb868ee
mrflick72 <valerio.vaudi@gmail.com>
2025-10-15 07:27:31 +0200

    partial

--- budget/budget-api/src/app/server.py
+++ budget/budget-api/src/app/server.py
@@ -1,9 +1,9 @@
 import os
 
 from flask import Flask
-from revenue.api.revenue_end_point import revenue_end_point
-from infrastructure.management.health_end_point import HealthEndPoint
-from infrastructure.middleware.user_name_injector_filter import UserNameInjectorFilter
+from app.revenue.api.revenue_end_point import revenue_end_point
+from app.infrastructure.management.health_end_point import HealthEndPoint
+from app.infrastructure.middleware.user_name_injector_filter import UserNameInjectorFilter
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path=os.getenv("BUDGET_API_CONFIG_FILE_LOCATION"))
@@ -11,8 +11,8 @@ load_dotenv(dotenv_path=os.getenv("BUDGET_API_CONFIG_FILE_LOCATION"))
 
 app = Flask(__name__)
 
-user_name_injector_filter = UserNameInjectorFilter()
-app.before_request(user_name_injector_filter.filter)
+# user_name_injector_filter = UserNameInjectorFilter()
+# app.before_request(user_name_injector_filter.filter)
 
 HealthEndPoint(app)
 app.register_blueprint(revenue_end_point)
---
+++ budget/budget-api/tests/revenue/api/test_revenue_end_point.py
@@ -0,0 +1,90 @@
+import pytest
+from app.server import app
+from pytest_mock import MockerFixture
+from app.user.domain.user import UserName
+from app.time.domain.date import Date
+from app.money.domain.money import Money
+from app.revenue.domain.revenue import Revenue, RevenueId
+
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+@pytest.fixture
+def user_name_resolver(mocker):
+    mocked_user_name_resolver_response = UserName("A_USER_NAME")
+
+    mocked_user_name_resolver = mocker.Mock()
+    mocked_user_name_resolver.get_user_name.return_value = (
+        mocked_user_name_resolver_response
+    )
+    
+    return mocked_user_name_resolver
+    
+""" 
+
+@revenue_end_point.route('/budget/revenue', methods=['GET'])
+def get_revenue():
+    return {} 
+
+
+@revenue_end_point.route('/budget/revenue', methods=['POST'])
+def save_revenue():
+    return {} 
+
+@revenue_end_point.route('/budget/revenue/<id>', methods=['PUT'])
+def update_revenue():
+    return {} 
+
+
+@revenue_end_point.route('/budget/revenue/<id>', methods=['DELETE'])
+def delete_revenue():
+    return {} 
+
+    public void addANewBudgetRevenue() throws Exception {
+    BudgetRevenue budgetRevenue = new BudgetRevenue(null, "USER", dateFor("10/10/2018"), Money.ONE, "A_NOTE");
+
+    given(userRepository.currentLoggedUserName()).willReturn(new UserName("USER"));
+
+    BudgetRevenueRepresentation budgetRevenueRepresentation = new BudgetRevenueRepresentation(null, "10/10/2018", "1.00", "A_NOTE");
+    mockMvc.perform(post("/budget/revenue")
+            .contentType(MediaType.APPLICATION_JSON)
+            .with(csrf())
+            .content(objectMapper.writeValueAsBytes(budgetRevenueRepresentation)))
+            .andExpect(status().isCreated());
+
+    verify(budgetRevenueRepository).save(budgetRevenue);
+    verify(userRepository).currentLoggedUserName();
+}
+"""
+
+
+def test_add_new_revenue(client, mocker: MockerFixture):
+    mocked_user_name_resolver_response = UserName("A_USER_NAME")
+
+    mocked_repository = mocker.Mock()
+
+    mocked_user_name_resolver = mocker.Mock()
+    mocked_user_name_resolver.get_user_name.return_value = (
+        mocked_user_name_resolver_response
+    )
+
+    response = client.post(
+        "/budget/revenue",
+        json={"date": "10/10/2018", "amount": "1.00", "note": "A_NOTE"},
+    )
+
+    assert response.status_code == 201
+
+    mocked_repository.get_user_name.assert_called()
+    mocked_user_name_resolver.assert_called_once_with(
+        Revenue(
+            id=None,
+            user_name=UserName("A_USER_NAME"),
+            registration_date=Date.iso_date_for("2018-10-10"),
+            amount=Money.money_for("1.00"),
+            note="A_NOTE",
+        )
+    )
