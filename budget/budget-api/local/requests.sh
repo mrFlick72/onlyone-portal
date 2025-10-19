ce200d81873c7ddea2ee028ad08626d611e5be3b
mrflick72 <valerio.vaudi@gmail.com>
2025-10-19 14:56:03 +0200

    clean up

--- budget/budget-api/local/requests.sh
+++
@@ -1,6 +0,0 @@
-curl -v -X POST -H "Content-Type: application/json" -d '{
-    "year": 2024,
-    "month": 6,
-    "department": "Sales",
-    "amount": 15000
-}' http://localhost:3030/budget/revenue 
