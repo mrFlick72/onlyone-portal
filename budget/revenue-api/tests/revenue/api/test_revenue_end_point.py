e3c2b24fbd87b68f4934178fd67c0a305c47e5b3
mrflick72 <valerio.vaudi@gmail.com>
2025-10-19 21:38:38 +0200

    test

--- budget/revenue-api/tests/revenue/api/test_revenue_end_point.py
+++ budget/revenue-api/tests/revenue/api/test_revenue_end_point.py
@@ -12,56 +12,29 @@ def client():
     with app.test_client() as client:
         yield client
 
-    
-""" 
 
-@revenue_end_point.route('/budget/revenue', methods=['GET'])
-def get_revenue():
-    return {} 
-
-
-@revenue_end_point.route('/budget/revenue', methods=['POST'])
-def save_revenue():
-    return {} 
-
-@revenue_end_point.route('/budget/revenue/<id>', methods=['PUT'])
-def update_revenue():
-    return {} 
-
-
-@revenue_end_point.route('/budget/revenue/<id>', methods=['DELETE'])
-def delete_revenue():
-    return {} 
-
-    public void addANewBudgetRevenue() throws Exception {
-    BudgetRevenue budgetRevenue = new BudgetRevenue(null, "USER", dateFor("10/10/2018"), Money.ONE, "A_NOTE");
-
-    given(userRepository.currentLoggedUserName()).willReturn(new UserName("USER"));
-
-    BudgetRevenueRepresentation budgetRevenueRepresentation = new BudgetRevenueRepresentation(null, "10/10/2018", "1.00", "A_NOTE");
-    mockMvc.perform(post("/budget/revenue")
-            .contentType(MediaType.APPLICATION_JSON)
-            .with(csrf())
-            .content(objectMapper.writeValueAsBytes(budgetRevenueRepresentation)))
-            .andExpect(status().isCreated());
-
-    verify(budgetRevenueRepository).save(budgetRevenue);
-    verify(userRepository).currentLoggedUserName();
-}
-"""
-
-@pytest.mark.skip(reason="to be fixed")
 def test_add_new_revenue(client, mocker: MockerFixture):
-    mocked_user_name_resolver_response = UserName("A_USER_NAME")
-
-    mocked_save_revenue_service = mocker.Mock()
-    mocked_save_revenue_service.save_revenue
-    mocked_repository = mocker.Mock()
-
-    mocked_user_name_resolver = mocker.Mock()
-    mocked_user_name_resolver.get_user_name.return_value = (
-        mocked_user_name_resolver_response
+    mocked_fromRepresentationToDomain = mocker.patch(
+        "app.revenue.api.revenue_converter.fromRepresentationToDomain",
+        return_value=Revenue(
+            id=None,
+            user_name=UserName("A_USER_NAME"),
+            date=Date.iso_date_for("2018-10-10"),
+            amount=Money.money_for("1.00"),
+            note="A_NOTE",
+        ),autospec=True
     )
+    
+
+    mocked_save_revenue_use_case = mocker.patch(
+        "app.revenue.domain.service.SaveRevenue", autospec=True
+    )
+
+    mocked_save_revenue_use_case_factory = mocker.patch(
+        "app.revenue.config.RevenueConfigurationProvider.get_save_revenue_service",
+        autospec=True,
+    )
+    mocked_save_revenue_use_case_factory.return_value = mocked_save_revenue_use_case
 
     response = client.post(
         "/budget/revenue",
@@ -70,12 +43,12 @@ def test_add_new_revenue(client, mocker: MockerFixture):
 
     assert response.status_code == 201
 
-    mocked_repository.get_user_name.assert_called()
-    mocked_user_name_resolver.assert_called_once_with(
+    mocked_fromRepresentationToDomain.assert_called_once()
+    mocked_save_revenue_use_case.save_revenue.assert_called_once_with(
         Revenue(
             id=None,
             user_name=UserName("A_USER_NAME"),
-            registration_date=Date.iso_date_for("2018-10-10"),
+            date=Date.iso_date_for("2018-10-10"),
             amount=Money.money_for("1.00"),
             note="A_NOTE",
         )
