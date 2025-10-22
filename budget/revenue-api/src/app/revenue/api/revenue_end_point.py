3fee35f843ed15d99fbd03ab5a2dd3e108596ae0
mrflick72 <valerio.vaudi@gmail.com>
2025-10-22 19:17:17 +0200

    revenue endpoint test fix

--- budget/revenue-api/src/app/revenue/api/revenue_end_point.py
+++ budget/revenue-api/src/app/revenue/api/revenue_end_point.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Response
 from app.revenue.config import RevenueConfigurationProvider
 from app.revenue.api.revenue_converter import from_representation_to_domain
 from app.revenue.api.representation import RevenueRepresentation
@@ -11,12 +11,12 @@ async def get_revenue():
     return {}
 
 
-@revenue_end_point_router.post("/budget/revenue", status_code=201)
+@revenue_end_point_router.post("/budget/revenue")
 async def save_revenue(representation: RevenueRepresentation):
     save_revenue_service = RevenueConfigurationProvider.get_save_revenue_service()
     revenue = from_representation_to_domain(representation)
     save_revenue_service.save_revenue(revenue)
-    return {}
+    return Response(status_code=201)
 
 
 @revenue_end_point_router.route("/budget/revenue/{id}", methods=["PUT"])
