7ab0e0c08732c8cba89c2c038ad2d965da444286
mrflick72 <valerio.vaudi@gmail.com>
2025-10-28 13:13:02 +0100

    move id generation into the service if it is none

--- budget/revenue-api/tests/revenue/domain/test_save_budget_revenue.py
+++ budget/revenue-api/tests/revenue/domain/test_save_budget_revenue.py
@@ -9,7 +9,7 @@ from app.time.domain.date import Date
 from app.revenue.domain.repository import RevenueRepository
 from app.revenue.domain.revenue import RevenueIdProvider
 
-def test_happy_path(mocker: MockerFixture):
+def test_create_new_revenue_happy_path(mocker: MockerFixture):
     revenue = Revenue(
         id=None,
         user_name=UserName("A_USER_NAME"),
@@ -33,3 +33,27 @@ def test_happy_path(mocker: MockerFixture):
 
     mocked_revenue_repository.save.assert_called_once_with(revenue)
     mocked_revenue_id_provider.generate_id.assert_called_once()
+
+def test_update_revenue_happy_path(mocker: MockerFixture):
+    revenue = Revenue(
+        id="generated-id",
+        user_name=UserName("A_USER_NAME"),
+        date=Date.iso_date_for("2018-10-10"),
+        amount=Money.money_for("1.00"),
+        note="A_NOTE",
+    )
+
+    mocked_revenue_repository = mocker.Mock(spec = RevenueRepository)
+    mocked_revenue_repository.save.return_value = None
+    
+    mocked_revenue_id_provider = mocker.Mock(spec = RevenueIdProvider)
+
+    uut = SaveRevenue(
+        repository=mocked_revenue_repository,
+        revenue_id_provider=mocked_revenue_id_provider
+        )
+
+    uut.save(revenue)
+
+    mocked_revenue_repository.save.assert_called_once_with(revenue)
+    mocked_revenue_id_provider.generate_id.assert_not_called()
