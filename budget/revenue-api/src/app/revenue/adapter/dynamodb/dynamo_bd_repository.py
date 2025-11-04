41fa555b192ba05e5218af28f357335226d4b06c
mrflick72 <valerio.vaudi@gmail.com>
2025-11-04 08:07:46 +0100

    save failing test

---
+++ budget/revenue-api/src/app/revenue/adapter/dynamodb/dynamo_bd_repository.py
@@ -0,0 +1,25 @@
+from app.revenue.domain.repository import RevenueRepository
+from app.revenue.domain.revenue import Revenue, RevenueId
+
+from app.user.domain.user import UserName
+from app.time.domain.date import Date
+
+
+class DynamoDbRevenueRepository(RevenueRepository):
+
+    def find_by_id(revenue_id: RevenueId) -> Revenue:
+        pass
+
+    def find_by_data_range(
+        user_name: UserName, start: Date, end: Date
+    ) -> list[Revenue]:
+        pass
+
+    def save(budgetRevenue: Revenue) -> Revenue:
+        pass
+
+    def update(budgetRevenue: Revenue):
+        pass
+
+    def delete(id: RevenueId):
+        pass
