f6bd455a11bdadc4b143f1d934501e24b6d4c476
mrflick72 <valerio.vaudi@gmail.com>
2025-11-04 08:07:46 +0100

    save failing test

--- budget/revenue-api/tests/revenue/adapter/dynamodb/test_dynamo_db_repository.py
+++ budget/revenue-api/tests/revenue/adapter/dynamodb/test_dynamo_db_repository.py
@@ -1,8 +1,16 @@
 import pytest
+import boto3
+from app.revenue.domain.revenue import Revenue, RevenueId
+from app.time.domain.date import Date
+from app.time.domain.year import Year
+from app.time.domain.month import Month
+from app.money.domain.money import Money
+from app.user.domain.user import UserName
+
+from app.revenue.adapter.dynamodb.dynamo_bd_repository import DynamoDbRevenueRepository
 
 
 def test_find_by_id():
-    
     pass
 
 
@@ -11,7 +19,30 @@ def test_find_by_data_range():
 
 
 def test_save():
-    pass
+    dynamo_client = boto3.client("dynamodb")
+
+    uut = DynamoDbRevenueRepository(dynamo_client)
+
+    revenue = Revenue(
+        id=None,
+        user_name=UserName("USER"),
+        amount=Money.money_for("1.00"),
+        date=Date.date_for("12/02/2000"),
+        note="A_NOTE",
+    )
+    expected_revenue = Revenue(
+        id=RevenueId("123-465"),
+        user_name=UserName("USER"),
+        amount=Money.money_for("1.00"),
+        date=Date.date_for("12/02/2000"),
+        note="A_NOTE",
+    )
+
+    uut.save(revenue)
+
+    actual = uut.find_by_id("123-456")
+    
+    assert expected_revenue == actual
 
 
 def test_update():
