885d7bd3ce8a01b2e6bc30dbfed9df341ccee898
mrflick72 <valerio.vaudi@gmail.com>
2025-11-08 18:40:46 +0100

    porting

--- budget/revenue-api/src/app/revenue/adapter/dynamodb/dynamo_db_repository.py
+++ budget/revenue-api/src/app/revenue/adapter/dynamodb/dynamo_db_repository.py
@@ -1,22 +1,58 @@
+import base64
 from app.revenue.domain.repository import RevenueRepository
-from app.revenue.domain.revenue import Revenue, RevenueId
+from app.revenue.domain.revenue import Revenue, RevenueId, RevenueIdProvider
 
 from app.user.domain.user import UserName
 from app.time.domain.date import Date
 
 
-class DynamoDyIdGenerator:
-    def budgetIdFrom(revenue: Revenue) -> RevenueId:
+class SaltGenerator:
+    def new_salt(self) -> str:
         pass
 
-    def partition_key_from(date: Date, user_name: UserName) -> str:
-        pass
 
-    def partition_key_from(id: RevenueId) -> str:
-        pass
+class UuidSaltGenerator(SaltGenerator):
 
-    def range_key_from(id: RevenueId) -> str:
-        pass
+    def new_salt(self):
+        import uuid
+
+        return RevenueId(content=str(uuid.uuid4()))
+
+
+class DynamoDbRevenueIdProvider(RevenueIdProvider):
+
+    def __init__(self, salt_generator: SaltGenerator):
+        super().__init__()
+        self.salt_generator = salt_generator
+
+    def generate_id(self, revenue: Revenue) -> RevenueId:
+        if revenue.id is None:
+            date = revenue.date
+            user_name = revenue.user_name
+            return RevenueId(
+                f"{self.__partition_key_from(date=date, user_name=user_name)}-{self.__range_key_from(date=date)}"
+            )
+        else:
+            return revenue.id
+
+    def __partition_key_from(self, date: Date, user_name: UserName) -> str:
+        year = date.content.year
+        user_name_content = user_name.content
+
+        partition_key = f"{year}_{user_name_content}"
+        utf_8_partition_key = partition_key.encode("utf-8")
+
+        return base64.b64encode(utf_8_partition_key).decode("ascii")
+
+    def __range_key_from(self, date: Date) -> str:
+        day_of_the_month = date.content.day
+        month = date.content.month
+        salt = self.salt_generator.new_salt()
+
+        range_key = f"{month}_{day_of_the_month}_{salt}"
+        utf_8_range_key = range_key.encode("utf-8")
+
+        return base64.b64encode(utf_8_range_key).decode("ascii")
 
 
 class DynamoDbRevenueRepository(RevenueRepository):
--- budget/revenue-api/tests/revenue/adapter/dynamodb/test_dynamo_db_repository.py
+++ budget/revenue-api/tests/revenue/adapter/dynamodb/test_dynamo_db_repository.py
@@ -1,59 +1,59 @@
-import pytest
-import boto3
-from app.revenue.domain.revenue import Revenue, RevenueId
-from app.time.domain.date import Date
-from app.time.domain.year import Year
-from app.time.domain.month import Month
-from app.money.domain.money import Money
-from app.user.domain.user import UserName
+# import pytest
+# import boto3
+# from app.revenue.domain.revenue import Revenue, RevenueId
+# from app.time.domain.date import Date
+# from app.time.domain.year import Year
+# from app.time.domain.month import Month
+# from app.money.domain.money import Money
+# from app.user.domain.user import UserName
 
-from app.revenue.adapter.dynamodb.dynamo_bd_repository import DynamoDbRevenueRepository
-
-
-def test_find_by_id():
-    pass
+# from app.revenue.adapter.dynamodb.dynamo_bd_repository import DynamoDbRevenueRepository
 
 
-def test_find_by_data_range():
-    pass
+# def test_find_by_id():
+#     pass
 
 
-def test_save():
-    dynamo_client = boto3.resource(
-        service_name="dynamodb",
-        region_name="eu-central-1",
-        aws_access_key_id="xxx",
-        aws_secret_access_key="xxx",
-        endpoint_url="http://localhost:4566",
-    )
-
-    uut = DynamoDbRevenueRepository(dynamo_client)
-
-    revenue = Revenue(
-        id=None,
-        user_name=UserName("USER"),
-        amount=Money.money_for("1.00"),
-        date=Date.date_for("12/02/2000"),
-        note="A_NOTE",
-    )
-    expected_revenue = Revenue(
-        id=RevenueId("123-465"),
-        user_name=UserName("USER"),
-        amount=Money.money_for("1.00"),
-        date=Date.date_for("12/02/2000"),
-        note="A_NOTE",
-    )
-
-    uut.save(revenue)
-
-    actual = uut.find_by_id("123-456")
-
-    assert expected_revenue == actual
+# def test_find_by_data_range():
+#     pass
 
 
-def test_update():
-    pass
+# def test_save():
+#     dynamo_client = boto3.resource(
+#         service_name="dynamodb",
+#         region_name="eu-central-1",
+#         aws_access_key_id="xxx",
+#         aws_secret_access_key="xxx",
+#         endpoint_url="http://localhost:4566",
+#     )
+
+#     uut = DynamoDbRevenueRepository(dynamo_client)
+
+#     revenue = Revenue(
+#         id=None,
+#         user_name=UserName("USER"),
+#         amount=Money.money_for("1.00"),
+#         date=Date.date_for("12/02/2000"),
+#         note="A_NOTE",
+#     )
+#     expected_revenue = Revenue(
+#         id=RevenueId("123-465"),
+#         user_name=UserName("USER"),
+#         amount=Money.money_for("1.00"),
+#         date=Date.date_for("12/02/2000"),
+#         note="A_NOTE",
+#     )
+
+#     uut.save(revenue)
+
+#     actual = uut.find_by_id("123-456")
+
+#     assert expected_revenue == actual
 
 
-def test_delete():
-    pass
+# def test_update():
+#     pass
+
+
+# def test_delete():
+#     pass
