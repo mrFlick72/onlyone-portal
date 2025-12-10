e7be1f3289627cb33a23516faac6179ee34a0645
mrflick72 <valerio.vaudi@gmail.com>
2025-12-10 23:45:28 +0100

    code migration start to use the context to bring user details like user name form the token to the rest of the code

--- tagging/tag-api/adapter/dynamodb/repository_test.go
+++ tagging/tag-api/adapter/dynamodb/repository_test.go
@@ -1,23 +1,45 @@
 package dynamodb
 
 import (
+	"context"
 	"testing"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/mrFlick72/onlyone-portal/tagging/tag-api/domain"
 )
+
+func newStubbedContext() *context.Context {
+	ctx := context.Background()
+	userName := domain.UserName("testuser")
+	user := &domain.User{UserName: &userName}
+	newCtx, _ := domain.SetCurrentUser(user, &ctx)
+	return newCtx
+}
+
 func newTagDynamoDBRepository() *TagDynamoDBRepository {
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider("xxx", "xxx", "xxx")),
+		config.WithRegion("eu-central-1"),
+		config.WithBaseEndpoint("http://localhost:8000"),
+	)
+
+	if err != nil {
+		panic("unable to load SDK config, " + err.Error())
+	}
+
 	return &TagDynamoDBRepository{
-		// Initialize with mock or test dependencies as needed	
+		// Initialize with mock or test dependencies as needed
 		TableName: "TestTagsTable",
-		UserRepository: nil,
-		Client:         nil,
+		Client:    dynamodb.NewFromConfig(cfg),
 	}
 }
 func TestSaveTag(t *testing.T) {
 	repo := newTagDynamoDBRepository()
 	tag := domain.Tag{Key: "exampleKey", Value: "exampleValue"}
 
-	err := repo.SaveTag(tag)
+	err := repo.SaveTag(newStubbedContext(), &tag)
 	if err != nil {
 		t.Errorf("Expected no error, got %v", err)
 	}
@@ -27,7 +49,7 @@ func TestGetTagBy(t *testing.T) {
 	repo := newTagDynamoDBRepository()
 	key := "exampleKey"
 
-	tag, err := repo.GetTagBy(key)
+	tag, err := repo.GetTagBy(newStubbedContext(), key)
 	if err != nil {
 		t.Errorf("Expected no error, got %v", err)
 	}
@@ -39,7 +61,7 @@ func TestGetTagBy(t *testing.T) {
 func TestFindAllTags(t *testing.T) {
 	repo := newTagDynamoDBRepository()
 
-	tags, err := repo.FindAllTags()
+	tags, err := repo.FindAllTags(newStubbedContext())
 	if err != nil {
 		t.Errorf("Expected no error, got %v", err)
 	}
--- tagging/tag-api/main.go
+++ tagging/tag-api/main.go
@@ -2,22 +2,16 @@ package main
 
 import (
 	"log"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mrFlick72/onlyone-portal/tagging/tag-api/web/api"
 )
 
 func main() {
 	// Create a Gin router with default middleware (logger and recovery)
 	r := gin.Default()
 
-	// Define a simple GET endpoint
-	r.GET("/ping", func(c *gin.Context) {
-		// Return JSON response
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	api.RegisterEndpoints(r)
 
 	// Start server on port 8080 (default)
 	// Server will listen on 0.0.0.0:8080 (localhost:8080 on Windows)
