85ec65a0dd40077ae91340ac86685ef55b7382b7
mrflick72 <valerio.vaudi@gmail.com>
2025-12-15 23:58:35 +0100

    add configuration, files and setup script to local development

---
+++ tagging/tag-api/test/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"errors"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
+
+func newDynamoDBClient() (*dynamodb.Client, error) {
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider("xxx", "xxx", "xxx")),
+		config.WithRegion("eu-central-1"),
+		config.WithBaseEndpoint("http://localhost:4566"),
+	)
+
+	if err != nil {
+		panic("unable to load SDK config, " + err.Error())
+	}
+
+	return dynamodb.NewFromConfig(cfg), err
+}
+
+func setupTestDynamoDBTable() error {
+	// Create table if not exists
+	client, _ := newDynamoDBClient()
+
+	TableName := ""
+	_, err := client.CreateTable(context.TODO(), &dynamodb.CreateTableInput{
+		TableName: aws.String(TableName),
+		AttributeDefinitions: []types.AttributeDefinition{
+			{
+				AttributeName: aws.String("UserName"),
+				AttributeType: types.ScalarAttributeTypeS,
+			},
+			{
+				AttributeName: aws.String("Key"),
+				AttributeType: types.ScalarAttributeTypeS,
+			},
+		},
+		KeySchema: []types.KeySchemaElement{
+			{
+				AttributeName: aws.String("UserName"),
+				KeyType:       types.KeyTypeHash,
+			},
+			{
+				AttributeName: aws.String("Key"),
+				KeyType:       types.KeyTypeRange,
+			},
+		},
+		BillingMode: types.BillingModePayPerRequest,
+	})
+	if err != nil {
+		var resourceInUseException *types.ResourceInUseException
+		if !errors.As(err, &resourceInUseException) {
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+	setupTestDynamoDBTable()
+}
