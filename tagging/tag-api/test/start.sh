70160a63db7a14d4d0af8274d7b059aa89875114
mrflick72 <valerio.vaudi@gmail.com>
2025-12-23 23:45:03 +0100

    user autorities claim extractio wip

--- tagging/tag-api/test/start.sh
+++ tagging/tag-api/test/start.sh
@@ -4,6 +4,8 @@ export AWS_ACCESS_KEY_ID="xxx"
 export AWS_SECRET_ACCESS_KEY="xxx"
 export AWS_REGION="us-east-1"
 export DYNAMODB_ENDPOINT="http://localhost:4566"
+export OAUTH2_ROLE="ROLE_USER"
+export JWKS_ENDPOINT="http://local.api.vauthenticator.com:9090/oauth2/jwks"
 
 go run main.go
 go run ../main.go
