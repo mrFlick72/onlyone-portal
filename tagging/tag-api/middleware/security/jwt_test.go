627c25460d394db3d599c6eea332b95e53f11ce7
mrflick72 <valerio.vaudi@gmail.com>
2025-12-26 22:47:36 +0100

    middleware test coverage with middleware bug fix: error case was not finalized

--- tagging/tag-api/middleware/security/jwt_test.go
+++ tagging/tag-api/middleware/security/jwt_test.go
@@ -1,14 +1,19 @@
 package security
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/gin-gonic/gin"
+	"github.com/lestrrat-go/jwx/v3/jwk"
 	"github.com/lestrrat-go/jwx/v3/jwt"
+	"github.com/mrFlick72/onlyone-portal/tagging/tag-api/internal/testutils"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGetClaimListFromToken(t *testing.T) {
-	tok, err := jwt.NewBuilder().Claim("authorities", []interface{}{"a","b"}).Build()
+	tok, err := jwt.NewBuilder().Claim("authorities", []interface{}{"a", "b"}).Build()
 	assert.NoError(t, err)
 
 	got := getClaimListFromToken(tok, "authorities")
@@ -23,4 +28,36 @@ func TestGetClaimFromToken(t *testing.T) {
 	got := getClaimFromToken(tok, "claim_name")
 	assert.NotNil(t, got)
 	assert.Equal(t, "a_claim", *got)
+}
+
+func TestWhenARequestIsAuthorized(t *testing.T) {
+	router := SetupAuthenticatedTestWebServer()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/sample", nil)
+	req.Header.Set("Authorization", testutils.CreateTestToken([]string{"ROLE_USER", "ROLE_ADMIN"}))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "{\"message\":\"sample endpoint\"}", w.Body.String())
+}
+
+func TestWhenARequestIsNoAuthorized(t *testing.T) {
+	router := SetupAuthenticatedTestWebServer()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/sample", nil)
+	req.Header.Set("Authorization", testutils.CreateTestToken([]string{}))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusForbidden, w.Code)
+}
+
+func SetupAuthenticatedTestWebServer() *gin.Engine {
+	keySet, _ := jwk.ParseString(testutils.JwkStr)
+	middleware := NewOAuth2Middleware(keySet, "ROLE_USER")
+
+	router := testutils.SetupTestWebServer([]gin.HandlerFunc{middleware})
+
+	return router
 }
