0f1e57de3824c374aa91fcf1ef454d31e39f0090
mrflick72 <valerio.vaudi@gmail.com>
2026-01-08 00:19:30 +0100

    start to generalize a golang web framework as core backbone

---
+++ core-services/golang-web-framework/middleware/security/user.go
@@ -0,0 +1,24 @@
+package security
+
+import (
+	"context"
+	"errors"
+)
+
+type UserName = string
+
+
+type User struct {
+	UserName *UserName
+	Authorities *[]string
+}
+
+func GetCurrentUser(ctx *context.Context) (*User, error) {
+	if v := (*ctx).Value("user"); v != nil {
+		user := v.(User)
+		return &user, nil
+	} else {
+		return nil, errors.New("no current user in context")
+	}
+}
+
---
+++ core-services/golang-web-framework/web/magangement/health_endpoint.go
@@ -0,0 +1,13 @@
+package magangement
+
+import "github.com/gin-gonic/gin"
+
+func RegisterEndpoints(r *gin.Engine) *gin.Engine {
+
+	// GET /api/tags — return all tags as JSON
+	r.GET("/management/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": "UP"})
+	})
+
+	return r
+}
