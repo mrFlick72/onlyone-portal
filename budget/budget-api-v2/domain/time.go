9b4b9ab9b40ad5195da01d9993b145189f612cf0
mrflick72 <valerio.vaudi@gmail.com>
2026-01-11 23:51:24 +0100

    time domain class

--- budget/budget-api-v2/domain/time.go
+++ budget/budget-api-v2/domain/time.go
@@ -5,7 +5,79 @@ type Date struct {
 }
 
 type Month struct {
-	// todo
+	Content int8
+}
+
+func JANUARY() Month {
+	return Month{
+		Content: 1,
+	}
+}
+
+func FEBRUARY() Month {
+	return Month{
+		Content: 2,
+	}
+}
+
+func MARCH() Month {
+	return Month{
+		Content: 3,
+	}
+}
+
+func APRIL() Month {
+	return Month{
+		Content: 4,
+	}
+}
+
+func MAY() Month {
+	return Month{
+		Content: 5,
+	}
+}
+
+func JUNE() Month {
+	return Month{
+		Content: 6,
+	}
+}
+
+func JULY() Month {
+	return Month{
+		Content: 7,
+	}
+}
+
+func AUGUST() Month {
+	return Month{
+		Content: 8,
+	}
+}
+
+func SEPTEMBER() Month {
+	return Month{
+		Content: 9,
+	}
+}
+
+func OCTOBER() Month {
+	return Month{
+		Content: 10,
+	}
+}
+
+func NOVEMBER() Month {
+	return Month{
+		Content: 11,
+	}
+}
+
+func DECEMBER() Month {
+	return Month{
+		Content: 12,
+	}
 }
 
 type Year struct {
