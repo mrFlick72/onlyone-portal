65ab0b8de08a6b91658049c11e3b48667a34b4cc
mrflick72 <valerio.vaudi@gmail.com>
2026-01-12 22:56:58 +0100

    scaffoald

--- budget/budget-api-v2/domain/money.go
+++ budget/budget-api-v2/domain/money.go
@@ -1,4 +1,23 @@
 package domain
 
+import "github.com/shopspring/decimal"
+
+var SCALE_PRECISION = 2
+var ZERO, _ = MoneyFor("0.00")
+var ONE, _ = MoneyFor("1.00")
+
 type Money struct {
+	content decimal.Decimal
+}
+
+func MoneyFor(amount string) (*Money, error) {
+	return nil, nil
+}
+
+func (m *Money) Plus(money *Money) *Money {
+	return nil
+}
+
+func (m *Money) StringifyAmount() string {
+	return ""
 }
