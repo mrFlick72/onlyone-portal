7af986781f46b208f8101d0ef102977031933087
mrflick72 <valerio.vaudi@gmail.com>
2026-01-17 18:21:56 +0100

    add delete budget expense use case

---
+++ budget/budget-api-v2/domain/delete_budget_Expense_test.go
@@ -0,0 +1,107 @@
+package domain
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/go-playground/assert/v2"
+	"github.com/mrflick72/onlyone-portal/core-services/golang-web-framework/middleware/security"
+)
+
+func TestWhenABudgetExpenseDeletionSucceed(t *testing.T) {
+
+	mockedRepository := new(BudgetExpenseRepositoryMock)
+	uut := DeleteBudgetExpense{
+		repository: mockedRepository,
+	}
+
+	UserName := "A_USER_NAME"
+	user := security.User{UserName: &UserName, Authorities: nil}
+	ctx := context.WithValue(context.TODO(), "user", user)
+
+	foundBudgetExpense := &BudgetExpense{
+		Id:       "A_BUDGET_ID",
+		UserName: "A_USER_NAME",
+	}
+	mockedRepository.On("FindFor", &ctx, "A_BUDGET_ID").Return(foundBudgetExpense, nil)
+	mockedRepository.On("Delete", &ctx, "A_BUDGET_ID").Return(nil)
+
+	err := uut.Execute(&ctx, "A_BUDGET_ID")
+
+	assert.Equal(t, nil, err)
+
+	mockedRepository.AssertCalled(t, "FindFor", &ctx, "A_BUDGET_ID")
+	mockedRepository.AssertCalled(t, "Delete", &ctx, "A_BUDGET_ID")
+}
+
+func TestWhenABudgetExpenseDeletionFails(t *testing.T) {
+
+	mockedRepository := new(BudgetExpenseRepositoryMock)
+	uut := DeleteBudgetExpense{
+		repository: mockedRepository,
+	}
+
+	UserName := "A_USER_NAME"
+	user := security.User{UserName: &UserName, Authorities: nil}
+	ctx := context.WithValue(context.TODO(), "user", user)
+
+	foundBudgetExpense := &BudgetExpense{
+		Id:       "A_BUDGET_ID",
+		UserName: "A_USER_NAME",
+	}
+	mockedRepository.On("FindFor", &ctx, "A_BUDGET_ID").Return(foundBudgetExpense, nil)
+	mockedRepository.On("Delete", &ctx, "A_BUDGET_ID").Return(fmt.Errorf("budget expense delete with the id %s failed", "A_BUDGET_ID"))
+
+	err := uut.Execute(&ctx, "A_BUDGET_ID")
+
+	assert.NotEqual(t, nil, err)
+
+	mockedRepository.AssertCalled(t, "FindFor", &ctx, "A_BUDGET_ID")
+	mockedRepository.AssertCalled(t, "Delete", &ctx, "A_BUDGET_ID")
+}
+
+func TestWhenABudgetExpenseDeleteDoesDoneNothingBecauseTheBudgetExpenseDoesNotExist(t *testing.T) {
+
+	mockedRepository := new(BudgetExpenseRepositoryMock)
+	uut := DeleteBudgetExpense{
+		repository: mockedRepository,
+	}
+
+	UserName := "A_USER_NAME"
+	user := security.User{UserName: &UserName, Authorities: nil}
+	ctx := context.WithValue(context.TODO(), "user", user)
+
+	mockedRepository.On("FindFor", &ctx, "A_BUDGET_ID").Return(nil, fmt.Errorf("budget expense with the id %s was not found", "A_BUDGET_ID"))
+
+	err := uut.Execute(&ctx, "A_BUDGET_ID")
+
+	assert.NotEqual(t, nil, err)
+
+	mockedRepository.AssertCalled(t, "FindFor", &ctx, "A_BUDGET_ID")
+	mockedRepository.AssertNotCalled(t, "Delete", &ctx, "A_BUDGET_ID")
+}
+
+func TestWhenABudgetExpenseDeleteFailsBecauseTheUserWasWrong(t *testing.T) {
+mockedRepository := new(BudgetExpenseRepositoryMock)
+	uut := DeleteBudgetExpense{
+		repository: mockedRepository,
+	}
+
+	UserName := "A_USER_NAME"
+	user := security.User{UserName: &UserName, Authorities: nil}
+	ctx := context.WithValue(context.TODO(), "user", user)
+
+	foundBudgetExpense := &BudgetExpense{
+		Id:       "A_BUDGET_ID",
+		UserName: "A_DIFFERENT_USER_NAME",
+	}
+	mockedRepository.On("FindFor", &ctx, "A_BUDGET_ID").Return(foundBudgetExpense, nil)
+
+	err := uut.Execute(&ctx, "A_BUDGET_ID")
+
+	assert.Equal(t, nil, err)
+
+	mockedRepository.AssertCalled(t, "FindFor", &ctx, "A_BUDGET_ID")
+	mockedRepository.AssertNotCalled(t, "Delete", &ctx, "A_BUDGET_ID")
+}
