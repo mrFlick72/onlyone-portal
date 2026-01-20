13180af2a484454388b31c0c538545247785db1c
mrflick72 <valerio.vaudi@gmail.com>
2026-01-20 18:34:56 +0100

    find budget expenseby date range

--- budget/budget-api-v2/domain/budget/expense/find_budget_expense_test.go
+++ budget/budget-api-v2/domain/budget/expense/find_budget_expense_test.go
@@ -35,41 +35,10 @@ func TestBudgetExpenseTotalBySearchTagsWithConstraints(t *testing.T) {
 
 	assert.NotEqual(t, nil, actual)
 	assert.Equal(t, nil, err)
-	/*
-	   given(userRepository.currentLoggedUserName())
-	           .willReturn(A_USER_NAME);
 
-	   lenient().when(searchTagRepository.findSearchTagBy("super-market")).thenReturn(new SearchTag("super-market", "super-market"));
-	   given(searchTagRepository.findSearchTagBy("dinner")).willReturn(new SearchTag("dinner", "dinner"));
-
-	   given(budgetExpenseRepository.findByDateRange(A_USER_NAME, Date.firstDateOfMonth(Month.FEBRUARY, Year.of(2018)),
-	           Date.lastDateOfMonth(Month.FEBRUARY, Year.of(2018)), "dinner", "super-market"))
-
-	           .willReturn(asList(new BudgetExpense(BudgetFixture.emptyBudgetExpenseId(), A_USER_NAME, Date.dateFor("15/02/2018"), Money.moneyFor("10"), "dinner", "dinner"),
-	                   new BudgetExpense(BudgetFixture.emptyBudgetExpenseId(), A_USER_NAME, Date.dateFor("01/02/2018"), Money.moneyFor("12.50"), "super-market", "super-market"),
-	                   new BudgetExpense(BudgetFixture.emptyBudgetExpenseId(), A_USER_NAME, Date.dateFor("05/02/2018"), Money.moneyFor("12.50"), "super-market", "super-market"),
-	                   new BudgetExpense(BudgetFixture.emptyBudgetExpenseId(), A_USER_NAME, Date.dateFor("04/02/2018"), Money.moneyFor("20"), "dinner", "dinner"),
-	                   new BudgetExpense(BudgetFixture.emptyBudgetExpenseId(), A_USER_NAME, Date.dateFor("03/02/2018"), Money.moneyFor("12.50"), "super-market", "super-market"),
-	                   new BudgetExpense(BudgetFixture.emptyBudgetExpenseId(), A_USER_NAME, Date.dateFor("02/02/2018"), Money.moneyFor("12.50"), "super-market", "super-market"),
-	                   new BudgetExpense(BudgetFixture.emptyBudgetExpenseId(), A_USER_NAME, Date.dateFor("01/02/2018"), Money.moneyFor("15"), "dinner", "dinner")));
-
-	   SpentBudget actual = new FindSpentBudget(userRepository, budgetExpenseRepository, searchTagRepository)
-	           .findBy(Month.FEBRUARY, Year.of(2018), asList("dinner", "super-market"));
-
-	   Map<SearchTag, Money> expected = Map.of(new SearchTag("super-market", "super-market"), Money.moneyFor("50.00"),
-	           new SearchTag("dinner", "dinner"), Money.moneyFor("45.00"));
-
-	   verify(userRepository).currentLoggedUserName();
-	   verify(searchTagRepository).findSearchTagBy("dinner");
-	   verify(searchTagRepository).findSearchTagBy("super-market");
-	   verify(budgetExpenseRepository)
-	           .findByDateRange(A_USER_NAME, Date.firstDateOfMonth(Month.FEBRUARY, Year.of(2018)),
-	                   Date.lastDateOfMonth(Month.FEBRUARY, Year.of(2018)), "dinner", "super-market");
-
-	   Assertions.assertEquals(expected, actual.totalForSearchTags());
-	   Assertions.assertEquals(actual.total(), Money.moneyFor("95.00"));
-	*/
-
+    mockedSearchTagRepository.AssertCalled(t, "GetTagBy", &ctx, "dinner")
+	mockedSearchTagRepository.AssertCalled(t, "GetTagBy", &ctx, "super-market")
+	mockedBudgetExpenseRepository.AssertCalled(t, "FindByDateRange", &ctx, "A_USER_NAME", safeDateFor("01/04/2018"), safeDateFor("30/04/2018"), []string{"dinner", "super-market"})
 }
 
 func TestBudgetExpenseTotalBySearchTagsWithoutConstraints(t *testing.T) {
