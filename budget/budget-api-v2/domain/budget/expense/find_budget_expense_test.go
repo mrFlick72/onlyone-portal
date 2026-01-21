39170a94b4bdb5ae6120f752aac551eb93dbee8b
mrflick72 <valerio.vaudi@gmail.com>
2026-01-21 07:58:46 +0100

    add find budget expense test coverage

--- budget/budget-api-v2/domain/budget/expense/find_budget_expense_test.go
+++ budget/budget-api-v2/domain/budget/expense/find_budget_expense_test.go
@@ -42,4 +42,33 @@ func TestBudgetExpenseTotalBySearchTagsWithConstraints(t *testing.T) {
 }
 
 func TestBudgetExpenseTotalBySearchTagsWithoutConstraints(t *testing.T) {
+		ctx := newUserContext()
+	mockedSearchTagRepository := new(SearchTagRepositoryMock)
+	mockedBudgetExpenseRepository := new(BudgetExpenseRepositoryMock)
+	uut := FindSpentBudget{
+		budgetExpenseRepository: mockedBudgetExpenseRepository,
+		searchTagRepository:     mockedSearchTagRepository,
+	}
+
+	mockedSearchTagRepository.On("GetTagBy", &ctx, "super-market").Return(&tags.SearchTag{Key: "super-market", Value: "super-market"}, nil)
+        mockedSearchTagRepository.On("GetTagBy", &ctx, "dinner").Return(&tags.SearchTag{Key: "dinner", Value: "dinner"}, nil)
+
+	mockedBudgetExpenseRepository.On("FindByDateRange", &ctx, "A_USER_NAME", safeDateFor("01/04/2018"), safeDateFor("30/04/2018"), []string{}).
+		Return(&[]BudgetExpense{
+			{Id: "1", UserName: "A_USER_NAME", Date: safeDateFor("15/04/2018"), Amount: safeMoneyFor("10.00"), Note: "dinner", Tag: "dinner"},
+			{Id: "2", UserName: "A_USER_NAME", Date: safeDateFor("01/04/2018"), Amount: safeMoneyFor("12.50"), Note: "super-market", Tag: "super-market"},
+			{Id: "3", UserName: "A_USER_NAME", Date: safeDateFor("05/04/2018"), Amount: safeMoneyFor("12.50"), Note: "super-market", Tag: "super-market"},
+			{Id: "4", UserName: "A_USER_NAME", Date: safeDateFor("04/04/2018"), Amount: safeMoneyFor("20.00"), Note: "dinner", Tag: "dinner"},
+			{Id: "5", UserName: "A_USER_NAME", Date: safeDateFor("03/04/2018"), Amount: safeMoneyFor("12.50"), Note: "super-market", Tag: "super-market"},
+			{Id: "6", UserName: "A_USER_NAME", Date: safeDateFor("02/04/2018"), Amount: safeMoneyFor("12.50"), Note: "super-market", Tag: "super-market"},
+			{Id: "7", UserName: "A_USER_NAME", Date: safeDateFor("01/04/2018"), Amount: safeMoneyFor("15.00"), Note: "dinner", Tag: "dinner"},
+		}, nil)
+	actual, err := uut.Execute(&ctx, date.APRIL(), date.NewYear(2018), []string{})
+
+	assert.NotEqual(t, nil, actual)
+	assert.Equal(t, nil, err)
+
+    mockedSearchTagRepository.AssertCalled(t, "GetTagBy", &ctx, "dinner")
+	mockedSearchTagRepository.AssertCalled(t, "GetTagBy", &ctx, "super-market")
+	mockedBudgetExpenseRepository.AssertCalled(t, "FindByDateRange", &ctx, "A_USER_NAME", safeDateFor("01/04/2018"), safeDateFor("30/04/2018"), []string{})
 }
