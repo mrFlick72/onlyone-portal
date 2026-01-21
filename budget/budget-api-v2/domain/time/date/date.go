de79610faff26e8f03be6207544b68230aa0c3db
mrflick72 <valerio.vaudi@gmail.com>
2026-01-21 07:59:15 +0100

    add first and last date edge case tests and validation

--- budget/budget-api-v2/domain/time/date/date.go
+++ budget/budget-api-v2/domain/time/date/date.go
@@ -18,11 +18,17 @@ func (d *Date) GetIsoFormattedDate() string {
 }
 
 func FirstDateOfMonth(month Month, year Year) (*Date, error) {
+	if month.Content < 1 || month.Content > 12 {
+		return nil, errors.New("invalid month value")
+	}
 	DateContent := time.Date(int(year.Content), time.Month(month.Content), 1, 0, 0, 0, 0, time.UTC)
 	return &Date{t: DateContent}, nil
 }
 
 func LastDateOfMonth(month Month, year Year) (*Date, error) {
+	if month.Content < 1 || month.Content > 12 {
+		return nil, errors.New("invalid month value")
+	}
 	lastDate := time.Date(int(year.Content), time.Month(month.Content)+1, 0, 0, 0, 0, 0, time.UTC)
 	return &Date{t: lastDate}, nil
 }
