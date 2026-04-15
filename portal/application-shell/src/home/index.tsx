c745202d3ee2f6ecafa6f03f345aafb3145ddbba
mrflick72 <valerio.vaudi@gmail.com>
2026-04-15 20:57:52 +0200

    token auto refresh via silent autologin

--- portal/application-shell/src/home/index.tsx
+++ portal/application-shell/src/home/index.tsx
@@ -1,5 +1,8 @@
 import {createRoot} from 'react-dom/client';
 import HomePage from "./HomePage";
+import {authenticationChecker} from "../auth/Authenticator";
+
+authenticationChecker()
 
 if (document.getElementById('app')) {
     const container = document.getElementById('app');
