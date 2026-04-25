6cb674332c2dc20711b93afb1332e521d9c9894a
mrflick72 <valerio.vaudi@gmail.com>
2026-04-25 15:13:42 +0200

    global menu

---
+++ portal/application-shell/src/components/menu/GlobalPageNavigation.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MenuItem from './MenuItem';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Money, Person } from '@mui/icons-material';
+
+const GlobalPageNavigation: React.FC = () => (
+    <>
+        <MenuItem icon={<ShoppingCartIcon />} text="Budget" link="/budget/expense/index" />
+        <MenuItem icon={<Money />} text="Revenue" link="/budget/revenue/index" />
+        <MenuItem icon={<Person />} text="Account" link="/account/index" />
+    </>
+);
+
+export default GlobalPageNavigation;
