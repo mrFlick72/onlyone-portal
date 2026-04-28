9b36cbf41b085aa1d0c26bc8ac50c6d9cd52fb0d
mrflick72 <valerio.vaudi@gmail.com>
2026-04-28 19:32:02 +0200

    add create a new Plan function

--- plan/plan-api/adapter/plan/db/repository.go
+++ plan/plan-api/adapter/plan/db/repository.go
@@ -1,9 +1,14 @@
 package db
 
 import (
+	"github.com/google/uuid"
+	"github.com/mrflick72/onlyone-portal/core-services/golang-web-framework/logging"
 	domainplan "github.com/mrflick72/onlyone-portal/plan/plan-service/domain/plan"
 	"github.com/mrflick72/onlyone-portal/plan/plan-service/domain/todo"
+	"github.com/mrflick72/onlyone-portal/plan/plan-service/pkg/database"
 )
+
+var logger = logging.GetLoggerInstanceForComponentByTypeName("PlanPostgresRepository")
 
 type PlanPostgresRepository struct {
 	ConnectionString string
@@ -26,7 +31,23 @@ func (r *PlanPostgresRepository) GetPlanDetails(idPlanId string, userName string
 }
 
 func (r *PlanPostgresRepository) CreateNewPlan(p domainplan.Plan) (string, error) {
-	return "", nil
+	db, err := database.GetDatabaseConnectionFor(r.ConnectionString)
+	if err != nil {
+		return "", err
+	}
+
+	id, _ := uuid.NewRandom()
+	planId := id.String()
+
+	query, err := db.Prepare("INSERT INTO plan (id, user_name, title, date) VALUES ($1, $2, $3, $4)")
+	if err != nil {
+		return "", err
+	}
+
+	_, err = query.Exec(planId, p.UserName, p.Title, p.Date)
+	logger.LogErrorFor(err)
+	database.CloseResources(nil, query, db)
+	return planId, err
 }
 
 func (r *PlanPostgresRepository) AddTodo(idPlanId string, t todo.Todo) error {
