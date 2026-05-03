8021c6c10917c8b774ea0e69c0e61c1fbbe682c9
mrflick72 <valerio.vaudi@gmail.com>
2026-05-03 18:17:55 +0200

    refactoring: renaming

--- core-services/golang-web-framework/web/server/oauth2_configurer.go
+++ core-services/golang-web-framework/web/server/oauth2_configurer.go
@@ -6,28 +6,28 @@ import (
 	"github.com/mrflick72/onlyone-portal/core-services/golang-web-framework/middleware/security"
 )
 
-type OAuth2WebServerConfigurer struct {
+type OAuth2Configurer struct {
 	wsp       *WebServerProvisioner
 	ctx       context.Context
 	cancelCtx context.CancelFunc
 }
 
-func NewOauth2WebServerConfigurer(wsp *WebServerProvisioner) WebServerConfigurer {
+func NewOAuth2Configurer(wsp *WebServerProvisioner) WebServerConfigurer {
 	ctx, cancel := context.WithCancel(context.Background())
-	configurer := &OAuth2WebServerConfigurer{
+	configurer := &OAuth2Configurer{
 		wsp:       wsp,
 		ctx:       ctx,
 		cancelCtx: cancel,
 	}
-	wsp.cancelContextFns = append(wsp.cancelContextFns, configurer)
+	wsp.configurers = append(wsp.configurers, configurer)
 	return configurer
 }
 
-func (configurer *OAuth2WebServerConfigurer) Name() string {
+func (configurer *OAuth2Configurer) Name() string {
 	return "oauth2"
 }
 
-func (configurer *OAuth2WebServerConfigurer) Configure() error {
+func (configurer *OAuth2Configurer) Configure() error {
 	oauth2, err := security.SetUpOAuth2(configurer.ctx)
 	if err != nil {
 		return fmt.Errorf("oauth2 setup: %w", err)
@@ -36,7 +36,7 @@ func (configurer *OAuth2WebServerConfigurer) Configure() error {
 	return nil
 }
 
-func (configurer *OAuth2WebServerConfigurer) Dispose() error {
+func (configurer *OAuth2Configurer) Dispose() error {
 	configurer.cancelCtx()
 	return nil
 }
