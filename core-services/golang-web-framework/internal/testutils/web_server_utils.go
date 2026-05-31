bed41f57cca90ee3bfa85aed5225b4048e818d84
mrflick72 <valerio.vaudi@gmail.com>
2026-05-31 14:02:33 +0200

    build failure fix

--- core-services/golang-web-framework/internal/testutils/web_server_utils.go
+++ core-services/golang-web-framework/internal/testutils/web_server_utils.go
@@ -16,18 +16,18 @@ import (
 )
 
 var JwkStr = `{
-		"kty": "RSA",
-		"kid": "test-key",
-		"alg": "RS256",
-		"n": "mmO0OvOPQ53HRxV4eHOkTTxLVfk6zcq8KAD86gbnydYBNO_Si4Q1twyvefd58-BaO4N4NCEA97QrYm57ThKCe8agLGwWPHhxgbu_SAuYQehXxkf4sWy7Q17kGFG5k5AfQGZBqTY-YaawQqLlF6ILVbWab_AoEF4yB7pI3AnNnXs",
-		"e": "AQAB",
-		"d": "RzsrI2vONJcuIyjPzVslehEQfRkhPWOFTjuudNc8yA25vs_LZ11XXx42M-KvXIqtdvngUsTLan2w6pgowcuecX3t_2wUx0GJJgARfkN7gsWIS3CyXZBEEMjLGVU4vHt5zNE3GJKo3hb1TwEiulpL_Ix6hfcTSJpEaBWrBxjxV-E",
-		"p": "5EA0bi6ui1H1wsG85oc7i9O7UH58WPIK_ytzBWXFIwcaSFFBqqNYNnZaHFsMe4cbHSBgShWHO3UueGVgOKmB8Q",
-		"q": "rSi7CosQZmj_RFIYW10ef7XTZsdpIdOXV9-1dThAJUvkslKiTfdU7T0IYYsJ2K58ekJqdpcoKAVLB2SZVvdqKw",
-		"dp": "S9yjEHPng1qsShzGQgB0ZBbtTOWdQpq_2OuCAStACFJWA-8t2h8MNJ3FeWMxlOTkuBuIpVbeaX6bAV0ATBTaoQ",
-		"dq": "ZssMJhkh1jm0d-FoVix0Y4oUAiqUzaDnciH6faiz47AnBnkporEV-HPH2ugII1qJyKZOvzHCg-eIf84HfWoI2w",
-		"qi": "lyVz1HI2b1IjzOMENkmUTaVEO6DM6usZi3c3_MobUUM05yyBhnHtPjWzqWn1uJ_Gt5bkJDdcpfvmkPAhKWEU9Q"
-	}`
+			"alg": "RS256",
+			"d": "DS-VAKWRHFx2UzpGJgYwCvG6QdMmBOsaJG0MH7Te2xkkjAr5wq3C20e2RuxVgvrluUk8neFFXAYyQHqbsbvT4bEgX9VoDa8ByUFTsLgIb8Q3w-9gIuUvr5930fnZSkQAXWpgemp3vQh09LdAgWs0tqOsafK6T5LeRroQaenLOebzp0PIMcF8qTromO-4Gpa1Kw8kPBt8UFlvuXwRHh03sLO-NvplLO2Kk43pLg_15_1HS4jSixZS28rzVConMU-KdvVBAlbA4wUxS3g2ylQQzd9YlWW_bJTs4HAs-5FBKGd714QWv9AH4uGtnt29QyBKDts3Ltqf4yTKl8jdxD2z4Q",
+			"dp": "YiyV0Wzut95PEm1kDcAYtqDEYHcN5MVDz-tf5mrvFbRS1NDmVVe_5-r8l_ClQTnCLAX56mKSDVBg95mhUbJLjX5_d3NW36K-bDKsQ0AmpuURSFoaMqvRuJTGifbCoE0pxpast1loOlDClhiPy9wUCvm27ljw3BOy0gW856yHcRE",
+			"dq": "wG9njVaAM1yDKCR6DZIoG79ndQWuboukKW2vIqFjegaUVeolxE5vWIr1mqIF3t0_-CT0yB1bx4BTVZOn-bcHUP1OxAhkLRTWd5Kr2gsDv978vRcJnCPdyIUX9AZbKEo2LmcETCUyNB5djY1HRQqhd8ghyKJtZJgi_L54INBYo1E",
+			"e": "AQAB",
+			"kid": "test-key",
+			"kty": "RSA",
+			"n": "rxmYcy9OstuH5p0qOeFfiqd6dXwOXzeE1P7FSAldt7d3BDCX911OXgwJhoWriwzp5jEXcGnHZbXdGXLkVMkWNw_w5zublLt9JCXCgihj5QI-vy48APNPAPRkCCBvAu7TAoqR4OZsGcpWjpSL1EHVBCgmx8qV5ia255laZfb0oyb_kWQ4DDJq6Qi1eIuhMVubrheCqi2bOqM_pOt6_LuOksSzWutp5XO0pg9vf4VzbMripqurwqxetK0Zx-1VgCxfp1USTWguwB0xK-6a1EjdGua-RVVQah2BZpV9g2O1PfWaeK88FX6uzSRbcl3N4YjO8kgemRfumHfOZz0iukwruQ",
+			"p": "55-jYUpM1NLZwryeGbt0yngXwGelRtzbF4es2hyevbH257kHjy3s3Vs5VOPdlie7yB2xcl_Go5YvcYJEMQ_hn1NNac5nIpyK0adrtwzwMfKev4nDxZbxKoIoWqjqOSx1VbsO94XBjOI9qZVoWLrTCTa0csraiZSbGfnCmRux5ok",
+			"q": "wYcbu7ehOUA8G0r5Q1ZFB2cu7xO-6mn1b9gO_eyIC4KlrU1IE1gVniM-81RdGxcrOrSLKakwxCbW0YsG8mJg1ZiAkqqQDx7O9Lfgd0LFCOSODNREqia7wQQXXs32Dk__yEzpj9HLwABSIk5Hp5p8L2nxRYlxIxkeykL_cXsLz7E",
+			"qi": "xsMoNcfCz9er-6_GMlx4_LclYGubqDPFl9xNlpsG0on8Cop0FiXAr9LKyCgUkcsRbExdNClTDDtqULqC8sC0V9vCUQjt1RjVJPUMgtN2JMAAMsnDgc7vxi5oCDOmxkRonl2foPUYUr7MB-J_jKlLmOHjYxYAWqHy_pl7w7r0kdM"
+		}`
 
 func SetupTestWebServer(middleware []gin.HandlerFunc) *gin.Engine {
 	router := gin.Default()
