b1f78a834579e211a89dc6e9068231d368317127
mrflick72 <valerio.vaudi@gmail.com>
2026-06-04 22:46:10 +0200

    add more test coverage

--- core-services/golang-web-framework/cypto/aws_key_repostory_test.go
+++ core-services/golang-web-framework/cypto/aws_key_repostory_test.go
@@ -1,64 +1,12 @@
 package cypto
 
 import (
-	"context"
 	"encoding/base64"
-	"fmt"
 	"sync"
 	"testing"
 
-	"github.com/aws/aws-sdk-go-v2/service/kms"
-	"github.com/aws/aws-sdk-go-v2/service/kms/types"
 	"github.com/go-playground/assert/v2"
 )
-
-func newAwsKmsKayRepositoryWithCache() *AwsKmsKayRepository {
-	keyId := "A_KEY_ID"
-	encryptedKeyValue := "AN_ENCRYTPED_KEY"
-
-	return &AwsKmsKayRepository{
-		kmsClient: newLocalKmsClient(),
-		storage:   map[string]string{keyId: encryptedKeyValue},
-		cache:     map[string][]byte{keyId: []byte(encryptedKeyValue)},
-		mu:        sync.RWMutex{},
-	}
-}
-
-func newAwsKmsKayRepository(storage map[string]string) *AwsKmsKayRepository {
-	return &AwsKmsKayRepository{
-		kmsClient: newLocalKmsClient(),
-		storage:   storage,
-		cache:     make(map[string][]byte, 0),
-		mu:        sync.RWMutex{},
-	}
-}
-func masterKeySetup() (string, []byte, []byte) {
-	kmsClient := newLocalKmsClient()
-
-	masterKey, err := kmsClient.CreateKey(context.TODO(), &kms.CreateKeyInput{
-		KeyUsage: types.KeyUsageTypeEncryptDecrypt,
-	})
-
-	if err != nil {
-		fmt.Printf("Error on key creation: %v", err)
-		panic("AWS KMS key generation error")
-	}
-
-	key, err := kmsClient.GenerateDataKey(context.TODO(),
-		&kms.GenerateDataKeyInput{
-			KeyId:   masterKey.KeyMetadata.KeyId,
-			KeySpec: types.DataKeySpecAes256,
-		})
-
-	if err != nil {
-		fmt.Printf("Error on data key creation: %v", err)
-		panic("AWS KMS data key generation error")
-	}
-
-	fmt.Println("key")
-	fmt.Println(key)
-	return *masterKey.KeyMetadata.KeyId, key.CiphertextBlob, key.Plaintext
-}
 
 func TestWhenAKeyIsInTheCache(t *testing.T) {
 	uut := newAwsKmsKayRepositoryWithCache()
@@ -72,7 +20,48 @@ func TestWhenAKeyIsNotInTheCache(t *testing.T) {
 	masterKeyId, encryptedKey, plainTextKey := masterKeySetup()
 	uut := newAwsKmsKayRepository(map[string]string{masterKeyId: base64.StdEncoding.EncodeToString(encryptedKey)})
 	actual, _ := uut.GetKeyFor(masterKeyId)
-	fmt.Println("actual")
-	fmt.Println(actual)
 	assert.Equal(t, plainTextKey, actual.content)
+}
+
+func TestWhenKeyIdIsNotFoundInStorage(t *testing.T) {
+	uut := newAwsKmsKayRepository(map[string]string{})
+	_, err := uut.GetKeyFor("MISSING_KEY")
+	assert.NotEqual(t, nil, err)
+}
+
+func TestWhenStoredCiphertextIsInvalidBase64(t *testing.T) {
+	uut := newAwsKmsKayRepository(map[string]string{"KEY": "not-base64!!!"})
+	_, err := uut.GetKeyFor("KEY")
+	assert.NotEqual(t, nil, err)
+}
+
+func TestCacheIsPopulatedAfterFirstCacheMiss(t *testing.T) {
+	masterKeyId, encryptedKey, plainTextKey := masterKeySetup()
+	uut := newAwsKmsKayRepository(map[string]string{masterKeyId: base64.StdEncoding.EncodeToString(encryptedKey)})
+
+	first, err1 := uut.GetKeyFor(masterKeyId)
+	second, err2 := uut.GetKeyFor(masterKeyId)
+
+	assert.Equal(t, nil, err1)
+	assert.Equal(t, nil, err2)
+	assert.Equal(t, plainTextKey, first.content)
+	assert.Equal(t, first.content, second.content)
+
+	_, inCache := uut.cache[masterKeyId]
+	assert.Equal(t, true, inCache)
+}
+
+func TestConcurrentAccessOnCachedKey(t *testing.T) {
+	uut := newAwsKmsKayRepositoryWithCache()
+	var wg sync.WaitGroup
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			key, err := uut.GetKeyFor("A_KEY_ID")
+			assert.Equal(t, nil, err)
+			assert.Equal(t, []byte("AN_ENCRYTPED_KEY"), key.content)
+		}()
+	}
+	wg.Wait()
 }
--- core-services/golang-web-framework/cypto/fixture.go
+++ core-services/golang-web-framework/cypto/fixture.go
@@ -6,11 +6,14 @@ package cypto
 
 import (
 	"context"
+	"fmt"
+	"sync"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	aws_config "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/kms"
+	"github.com/aws/aws-sdk-go-v2/service/kms/types"
 )
 
 const localStackEndpoint = "http://localhost:4566"
@@ -29,4 +32,52 @@ func newLocalKmsClient() *kms.Client {
 		panic("unable to load SDK config, " + err.Error())
 	}
 	return kms.NewFromConfig(cfg)
+}
+
+
+func newAwsKmsKayRepositoryWithCache() *AwsKmsKayRepository {
+	keyId := "A_KEY_ID"
+	encryptedKeyValue := "AN_ENCRYTPED_KEY"
+
+	return &AwsKmsKayRepository{
+		kmsClient: newLocalKmsClient(),
+		storage:   map[string]string{keyId: encryptedKeyValue},
+		cache:     map[string][]byte{keyId: []byte(encryptedKeyValue)},
+		mu:        sync.RWMutex{},
+	}
+}
+
+func newAwsKmsKayRepository(storage map[string]string) *AwsKmsKayRepository {
+	return &AwsKmsKayRepository{
+		kmsClient: newLocalKmsClient(),
+		storage:   storage,
+		cache:     make(map[string][]byte, 0),
+		mu:        sync.RWMutex{},
+	}
+}
+
+func masterKeySetup() (string, []byte, []byte) {
+	kmsClient := newLocalKmsClient()
+
+	masterKey, err := kmsClient.CreateKey(context.TODO(), &kms.CreateKeyInput{
+		KeyUsage: types.KeyUsageTypeEncryptDecrypt,
+	})
+
+	if err != nil {
+		fmt.Printf("Error on key creation: %v", err)
+		panic("AWS KMS key generation error")
+	}
+
+	key, err := kmsClient.GenerateDataKey(context.TODO(),
+		&kms.GenerateDataKeyInput{
+			KeyId:   masterKey.KeyMetadata.KeyId,
+			KeySpec: types.DataKeySpecAes256,
+		})
+
+	if err != nil {
+		fmt.Printf("Error on data key creation: %v", err)
+		panic("AWS KMS data key generation error")
+	}
+
+	return *masterKey.KeyMetadata.KeyId, key.CiphertextBlob, key.Plaintext
 }
